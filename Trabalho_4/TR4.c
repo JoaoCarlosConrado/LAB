64e362712918220c06afc2119fd6c3a90f30a51d
JoaoCarlosConrado <conrado0804@gmail.com>
2022-10-04 08:20:30 -0300

    Added TR4.c

---
+++ Trabalho_4/TR4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ordvetor.h"
+
+typedef int COMP(void* x, void* y){
+    
+}
+
+VETORORD* VETORD_create(int n, COMP* compara){
+
+}
+
+void VETORD_add(VETORORD* vetor, void* newelem){
+
+}
+
+void* VETORD_remove(VETORORD* vetor){
+
+}
