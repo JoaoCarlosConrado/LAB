93a5909d35e5add1c27d895b97ee86ba2d6ffe03
JoaoCarlosConrado <conrado0804@gmail.com>
2022-10-05 11:00:23 -0300

    Updated TR4

--- Trabalho_4/src/TR4.c
+++ Trabalho_4/src/TR4.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include "../lib/ordvetor.h"
 
-typedef int COMP(void* x, void* y){
-
-}
-
 VETORORD* VETORD_create(int n, COMP* compara){
-    
+    VETORORD* vetorord = malloc(sizeof(VETORORD));
+    vetorord->comparador = compara;
+    vetorord->elems = malloc(sizeof(vetorord->elems));
+    vetorord->N = n;
+    vetorord->P = 0;
+    for(int i = 0;i<n;i++){
+        vetorord->elems[i] = NULL;
+    }
+    return vetorord;
 }
 
 void VETORD_add(VETORORD* vetor, void* newelem){
-
+    
 }
 
 void* VETORD_remove(VETORORD* vetor){
--- Trabalho_4/src/main.c
+++ Trabalho_4/src/main.c
@@ -3,8 +3,9 @@
 #include "../lib/ordvetor.h"
 
 int main(){
+    VETORORD* veto = VETORD_create(5, NULL);
 
-    
-
+    veto->elems[8] = "OI, eu sou o Goku!";
+    printf("%s", veto->elems[8]);
     return 0;
 }
