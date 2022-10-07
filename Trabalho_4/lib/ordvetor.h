be855751f74ce3845546b70eb76c09883daff789
JoaoCarlosConrado <conrado0804@gmail.com>
2022-10-07 12:18:14 -0300

    Updated ordvetor.h

--- Trabalho_4/lib/ordvetor.h
+++ Trabalho_4/lib/ordvetor.h
@@ -4,10 +4,17 @@
 #ifndef ORDVETOR_H
 #define ORDVETOR_H
 
-//Definicao do tipo de funcao a ser utilizado para comparacao
+/*Definicao do tipo de funcao a ser utilizado para comparacao
+Esta função recebe dois valores x e y e retorna
+
+1 : se x é menor que y.
+0 : se x é equivalente a y.
+-1: se x é maior que y na ordem.
+
+*/
 typedef int COMP(void* x, void* y);
 
-//Vetor ordenado estatico. Os elementos do vetor estao colocados no vetor
+//Vetor ordenado estatico. Os elementos do vetor estao colocadoo no vetor
 //de acordo com a ordem indicada por comparador.
 typedef struct vet{
 	int N;//tamanho do vetor
@@ -28,16 +35,17 @@ deve apontar para a estrutura, ou seja, a alocacao de memoria deve ser feita nel
 VETORORD* VETORD_create(int n, COMP* compara);
 
 /**
-Adiciona o elemento newelem ao vetor ordenado, na posicao correta se for possível
-ainda incluir o elemento (se existe espaco no vetor).
+Adiciona o elemento newelem ao vetor ordenado, na posicao correta se for possí­vel
+ainda incluir o elemento (se existe espaco no vetor). O vetor deve estar ordenado em
+ordem crescente, ou seja: x[i] é menor que ou equivalente a x[j] para todo i < j.
 
-@param vetor: o vetor ordenado a ter o elemento incluído
+@param vetor: o vetor ordenado a ter o elemento incluÃ­do
 @param newelem: o elemento a ser adicionado
 */
 void VETORD_add(VETORORD* vetor, void* newelem);
 
 /**
-Remove o menor elemento do vetor, de acordo com a funcao compara
+Remove o menor elemento do vetor, de acordo com a funcao compara.
 
 @param vetor: o vetor a ter seu elemento removido
 
