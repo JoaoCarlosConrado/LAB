4a75f56ec21c6accc350cc53e3218d0c97d62d0c
JoaoCarlosConrado <conrado0804@gmail.com>
2022-10-06 21:36:33 -0300

    Updated Trabalho_4

--- Trabalho_4/src/TR4.c
+++ Trabalho_4/src/TR4.c
@@ -5,7 +5,6 @@
 VETORORD* VETORD_create(int n, COMP* compara){
     VETORORD* vetorord = malloc(sizeof(VETORORD));
     vetorord->comparador = compara;
-    vetorord->elems = malloc(sizeof(vetorord->elems));
     vetorord->N = n;
     vetorord->P = 0;
     for(int i = 0;i<n;i++){
@@ -15,9 +14,33 @@ VETORORD* VETORD_create(int n, COMP* compara){
 }
 
 void VETORD_add(VETORORD* vetor, void* newelem){
-    
+    if(vetor->P < vetor->N){
+        for(int i = 0;i<=vetor->P;i++){
+            if(vetor->P == 0){
+                vetor->elems[vetor->P] = newelem;
+                vetor->P++;
+                break;
+            }else if(vetor->comparador(newelem, vetor->elems[vetor->P-1]) == 1){
+                vetor->elems[vetor->P] = newelem;
+                vetor->P++;
+                break;
+            }else if((vetor->comparador(newelem, vetor->elems[i]) == -1)||(vetor->comparador(newelem, vetor->elems[i]) == 0)){
+                for(int j = vetor->P;j>i;j--){
+                    vetor->elems[j] = vetor->elems[j-1];
+                }
+                vetor->elems[i] = newelem;
+                vetor->P++;
+                break;
+            }
+        }
+    }
 }
 
 void* VETORD_remove(VETORORD* vetor){
-
+    void* aux = vetor->elems[0];
+    for(int i = 0;i<vetor->P;i++){
+        vetor->elems[i] = vetor->elems[i+1]; 
+    }
+    vetor->P--;
+    return aux;
 }
--- Trabalho_4/src/main.c
+++ Trabalho_4/src/main.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include "../lib/ordvetor.h"
 
-int main(){
-    VETORORD* veto = VETORD_create(5, NULL);
 
-    veto->elems[8] = "OI, eu sou o Goku!";
-    printf("%s", veto->elems[8]);
+int comparacao(void* x, void* y){
+    if((int*)x == (int*)y){
+        return 0;
+    }else if((int*)x > (int*)y){
+        return 1;
+    }else{
+        return -1;
+    }
+}
+
+int main(){
+    COMP* comp = comparacao;
+    VETORORD* veto = VETORD_create(5, comp);
+    
+    VETORD_add(veto, (int*)5);
+    VETORD_add(veto, (int*)2);
+    VETORD_add(veto, (int*)3);
+    VETORD_add(veto, (int*)1);
+    VETORD_add(veto, (int*)4);
+    printf("%d removido\n", VETORD_remove(veto));
+    for(int i = 0;i<veto->P;i++){
+        printf("Elemento:%d %d\n",i, veto->elems[i]);
+    }
+    
     return 0;
 }
