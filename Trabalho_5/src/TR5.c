3bb6ca55c1f30c81ee7a6aa136ba3dac1f847b77
JoaoCarlosConrado <conrado0804@gmail.com>
2022-10-21 12:02:55 -0300

    updated remove

--- Trabalho_5/src/TR5.c
+++ Trabalho_5/src/TR5.c
@@ -23,7 +23,7 @@ void HEAP_add(HEAP* vetor, void* newelem){
         else{
             vetor->elems[vetor->P] = newelem;
             vetor->P++;
-            for(int i=vetor->P-1;i>0;i--){
+            for(int i=vetor->P-1;i>0;i = i/2){
                 if(vetor->comparador(vetor->elems[i], vetor->elems[i/2]) == 1){
                     void* aux = vetor->elems[i];
                     vetor->elems[i] = vetor->elems[i/2];
@@ -34,18 +34,34 @@ void HEAP_add(HEAP* vetor, void* newelem){
     }
 }
 
-void* HEAP_remove(HEAP* vetor){
-    void* aux = vetor->elems[0];
-    for(int i = 0;i<vetor->P;i++){
-        vetor->elems[i] = vetor->elems[i+1]; 
-    }
-    vetor->P--;
-    for(int i=vetor->P-1;i>0;i--){
-        if(vetor->comparador(vetor->elems[i], vetor->elems[i/2]) == 1){
-            void* aux = vetor->elems[i];
-            vetor->elems[i] = vetor->elems[i/2];
-            vetor->elems[i/2] = aux;
+void descer(HEAP* veto, int pos){
+    int P = pos;
+    int N = veto->P;
+    void** vet = veto->elems;
+    COMP* comp= veto->comparador;
+
+    if(2*P < N){
+        int son = 2*P;
+        if(2*P+1<N){
+            if(comp(vet[2*P], vet[2*P+1]) == 1)
+                son = 2*P;
+            else son = 2*P+1;
+        }
+        if(comp(vet[son], vet[P]) == 1){
+            void* aux = vet[son];
+            vet[son] = vet[P];
+            vet[P] = aux;
+            descer(veto, son);
         }
     }
-    return aux;
+}
+
+void* HEAP_remove(HEAP* vetor){
+    void* aux = vetor->elems[0];
+    vetor->elems[0] = vetor->elems[vetor->P-1];
+    vetor->elems[vetor->P-1] = aux;
+    vetor->P--;
+    descer(vetor, 0);
+    void* ret = vetor->elems[vetor->P];
+    return ret;
 }
